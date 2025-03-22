43993fdfad853daae9b6d583a64244112e5a9283
Daniel Yaghil <126357938+danielyaghil@users.noreply.github.com>
2025-03-22 12:49:52 +0200

    Update settings-set-location-step.js

--- src/models/steps/settings-set-location-step.js
+++ src/models/steps/settings-set-location-step.js
@@ -1,7 +1,6 @@
 const BaseStep = require('./base-step');
 const { replaceVariables } = require('../../helpers/utils');
 const { TestRunnerError } = require('../../helpers/test-errors');
-const { checkAppIsInstalled } = require('../../helpers/mobile-utils');
 
 class SetLocationStep extends BaseStep {
     constructor(sequence, step) {
@@ -27,29 +26,14 @@ class SetLocationStep extends BaseStep {
                 );
             }
 
-            if (this.conf.platformName.toLowerCase() === 'android') {
-                const isIoAppiumSettingAppInstalled = await checkAppIsInstalled(driver, 'io.appium.settings');
-                if (!isIoAppiumSettingAppInstalled) {
-                    console.log(
-                        `SetGeoLocation::App "io.appium.settings" is not installed on the device - installing it now`
-                    );
-                    await driver.execute('mobile: install', './apps/settings_apk-debug.apk');
-                    console.log(`SetGeoLocation::Installed app "io.appium.settings"`);
-                }
-
-                console.log(`SetGeoLocation::Allowing mock location for app "io.appium.settings"`);
-                let command = 'appops set io.appium.settings android:mock_location allow';
-                await driver.execute('mobile: shell', { command: command });
-
-                console.log(
-                    `SetGeoLocation::Starting location service for app "io.appium.settings" with latitude: ${latitude}, longitude: ${longitude}m altitude: ${altitude}`
-                );
-                command = `am start-foreground-service --user 0 -n io.appium.settings/.LocationService --es longitude ${longitude} --es latitude ${latitude} ${altitude != 0 ? '--es altitude ' + altitude : ''}`;
-                await driver.execute('mobile: shell', { command: command });
-
-                console.log(
-                    `SetGeoLocation::Set geo location to latitude: ${latitude}, longitude: ${longitude}, altitude: ${altitude}`
-                );
+            if (this.conf.runType === 'web') {
+                await driver.emulate('geolocation', {
+                    latitude: Number(latitude),
+                    longitude: Number(longitude),
+                    accuracy: 100
+                });
+            } else if (this.conf.platformName.toLowerCase() === 'android') {
+                await driver.execute('mobile:setGeolocation', { latitude, longitude, altitude });
             } else {
                 throw new TestRunnerError('SetGeoLocation::Setting geo location is not supported on iOS yet');
             }
